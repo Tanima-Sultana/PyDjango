9f2eea3d9908af9e1592ae6139ccb68ed934a0af
Tanima <tanimasultana.du@gmail.com>
2022-09-04 17:07:30 +0600

    the database added with forien key and relations

--- django_course_site/meetups/views.py
+++ django_course_site/meetups/views.py
@@ -1,7 +1,26 @@
 from http.client import HTTPResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+# from .models import Meetup
+
+meetups = [{'title':"aa",'slug':"ss",'location':"gg"}]
+#Meetup.objects.all()
 
 def index(request):
-    return render(request,'meetups/index.html')
+    return render(request,'meetups/index.html',{
+        'meetups':meetups
+    })
 
+def meetup_details(request,meetup_slug):
+    try:
+        selected_meetups = {'title':"aaa",'description':"des"}
+        # Meetup.objects.get(slug=meetup_slug)
+        return render(request,'meetups/meetup-details.html',{
+            'meetup_found':True,
+            'meetup_title': selected_meetups.title,
+            'meetup_description':selected_meetups.description
+        })
+    except Exception as exc:
+        return render(request,'meetups/meetup-details.html',{
+            'meetup_found':False
+        })
