da135fbfc648ea5351b6f024d53e6f369d4914ec
tanimasultana.du <tanimasultana.du@gmail.com>
2022-09-03 17:34:20 -0700

    added all files with html and css

---
+++ django_course_site/meetups/views.py
@@ -0,0 +1,7 @@
+from http.client import HTTPResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request,'meetups/index.html')
+
