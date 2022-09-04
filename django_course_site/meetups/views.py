6827b7ef21d4e6900b10a46d935b3d3092353232
tanimasultana.du <tanimasultana.du@gmail.com>
2022-09-04 10:49:37 -0700

    registration form added

--- django_course_site/meetups/views.py
+++ django_course_site/meetups/views.py
@@ -1,25 +1,36 @@
 from http.client import HTTPResponse
+from urllib.request import Request
 from django.shortcuts import render
 from django.http import HttpResponse
-# from .models import Meetup
+from .models import Meetup
+from .forms import RegistrationForm
 
-meetups = [{'title':"aa",'slug':"ss",'location':"gg"}]
-#Meetup.objects.all()
+meetups = Meetup.objects.all()
 
 def index(request):
+    print(meetups)
     return render(request,'meetups/index.html',{
         'meetups':meetups
     })
 
 def meetup_details(request,meetup_slug):
     try:
-        selected_meetups = {'title':"aaa",'description':"des"}
-        # Meetup.objects.get(slug=meetup_slug)
+        selected_meetups = Meetup.objects.get(slug=meetup_slug)
+        if request.method == 'GET':
+            registration_form = RegistrationForm()
+        else:
+            registration_form = RegistrationForm(Request.POST)
+            if registration_form.is_valid():
+                participant = registration_form.save()
+                selected_meetups.participants.add(participant)
+                
+
         return render(request,'meetups/meetup-details.html',{
-            'meetup_found':True,
-            'meetup_title': selected_meetups.title,
-            'meetup_description':selected_meetups.description
-        })
+                'meetup_found':True,
+                'meetup':selected_meetups,
+                'form':registration_form
+            })
+            
     except Exception as exc:
         return render(request,'meetups/meetup-details.html',{
             'meetup_found':False
